6e9b02721afc24409efee259f8b6ff4d27955718
Balcus <bbalcus04@gmail.com>
2026-06-14 01:01:57 +0300

    Cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,28 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Input source code files
+    files: Vec<String>,
+
+    /// Output destination for the produced binary
+    #[arg(short, long, value_name = "OUTPUT")]
+    output: Option<String>,
+
+    /// Lexer debug mode
+    #[arg(short = 'l')]
+    lexer_debug: bool,
+
+    /// Verbose lexer mode
+    #[arg(short = 'v')]
+    lexer_verbose: bool,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    for _file in &cli.files {
+        todo!();
+    }
+}
