c09692406c725d7bd6b38b03c8ae5fb8ecb40004
Balcus <bbalcus04@gmail.com>
2026-06-14 19:54:45 +0300

    Added string table impl & moved tests to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use clap::Parser;
+use coolc::lexer::{LexerExtras, Token};
+use logos::Logos;
+use std::fs;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// Input source code files
-    files: Vec<String>,
+    /// Paths for the input source files
+    paths: Vec<String>,
 
     /// Output destination for the produced binary
     #[arg(short, long, value_name = "OUTPUT")]
@@ -21,8 +24,20 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
-
-    for _file in &cli.files {
-        todo!();
+    for path in &cli.paths {
+        let s = fs::read_to_string(path).expect(&format!("Failed to read file: {path}"));
+        if cli.lexer_verbose {
+            let mut lexer = Token::lexer_with_extras(&s, LexerExtras::default());
+            for res in lexer.by_ref() {
+                match res {
+                    Ok(token) => println!("{:#?}", token),
+                    Err(e) => println!("Failed to read token {:?}", e),
+                }
+            }
+            println!("\nString table:");
+            for (string, id) in &lexer.extras.s_table.map {
+                println!("  {id}: {string:?}");
+            }
+        }
     }
 }
