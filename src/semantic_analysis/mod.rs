87934722d3cb73ec0bc087d07d7e77824880f216
Balcus <bbalcus04@gmail.com>
2026-07-18 17:49:17 +0300

    Started implementing the symbol table

--- src/semantic_analysis/mod.rs
+++ src/semantic_analysis/mod.rs
@@ -1,3 +1,12 @@
+use crate::{
+    semantic_analysis::{inheritance_tree::InheritanceTree, symbol_table::SymbolTable},
+    string_table::StringTable,
+};
+
+type SymbolId = usize;
+type TypeId = usize;
+type ClassId = usize;
+
 pub mod inheritance_tree;
 pub mod symbol_table;
 
@@ -6,4 +15,32 @@ pub enum SemanticError {
     InheritanceCycle,
     DuplicateClass,
     NonExistentClass(usize),
+}
+
+pub enum VarKind {
+    Local,
+    Formal,
+    Attribute,
+    SelfObject,
+}
+
+pub struct VarInfo {
+    ty: TypeId,
+    kind: VarKind,
+}
+
+pub struct MethodInfo {
+    ret_ty: TypeId,
+    formal_info: Vec<FormalInfo>,
+}
+
+pub struct FormalInfo {
+    ty: TypeId,
+}
+
+pub struct Environment<'a> {
+    string_table: &'a StringTable,
+    obejct_env: &'a mut SymbolTable<SymbolId, VarInfo>,
+    inheritance_tree: &'a InheritanceTree,
+    current_class: ClassId,
 }
